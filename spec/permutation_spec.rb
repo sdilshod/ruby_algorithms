6a7efa2f784ad49c2ab33ef75fcb26a831e4de78
sdilshod <samatov.dilshod@gmail.com>
2012-06-07 09:05:30 +0600

    Update master

--- spec/permutation_spec.rb
+++ spec/permutation_spec.rb
@@ -8,37 +8,107 @@ describe RubyAlgorithms::Permutation do
 #  end
   
   describe "#next_permutation" do
-    it "should generate next permutation for 12345" do
-      @obj_per = RubyAlgorithms::Permutation.new "12345"  
+    it "should generate next permutation for 12345 with lexicographical method" do
+      @obj = RubyAlgorithms::Permutation.new "12345"  
 
-      n_p = @obj_per.next_permutation
-      n_p.should == "12354"    
-      @obj_per.current_permutation= "12354"
-      n_p = @obj_per.next_permutation
-      n_p.should == "12435"    
+      @obj.next_permutation
+      @obj.get_current_permutation.should == "12354"    
 
-      @obj_per.current_permutation= "12435"
-      n_p = @obj_per.next_permutation
-      n_p.should == "12453"    
+      @obj.permutation_string= "12354"
+      @obj.next_permutation
+      @obj.get_current_permutation.should == "12435"    
 
-      @obj_per.current_permutation= "12453"
-      n_p = @obj_per.next_permutation
-      n_p.should == "12534"    
+      @obj.permutation_string = "12435"
+      @obj.next_permutation
+      @obj.get_current_permutation.should == "12453"    
+
+      @obj.permutation_string = "12453"
+      @obj.next_permutation
+      @obj.get_current_permutation.should == "12534"    
+    end
+
+    it "should generate next permutation for 12345 with recurtion method" do
+      @obj = RubyAlgorithms::Permutation.new "12345", true
+      @obj.next_permutation
+
+      @obj.get_current_permutation.should == "12354"
+
+      @obj.permutation_string = "12354"; @obj.next_permutation
+      @obj.get_current_permutation.should == "12534"
+
+      @obj.permutation_string = "12534"; @obj.next_permutation
+      @obj.get_current_permutation.should == "15234"
+
     end
   end
   
   describe "#turn" do
-    it "should roll and return all of permutations" do
-      @obj_per = RubyAlgorithms::Permutation.new "122"
-      arr_permutation = @obj_per.turn
-      arr_permutation.length.should == 3
-      arr_permutation.should be_include("212")
-      arr_permutation.should be_include("221")
+    it "should roll and return all of permutations with lexicographical method" do
+      @obj_per = RubyAlgorithms::Permutation.new "1234"
+      arr_permutations = @obj_per.turn
+      arr_permutations.should be_include("1243")
+      arr_permutations.should be_include("1423")
+      arr_permutations.should be_include("4123")
+      arr_permutations.should be_include("4132")
+    end
 
-      @obj_per.current_permutation = "12344"
-      arr_permutation = @obj_per.turn
-      arr_permutation.should be_include("44321")
+    it "should roll and return all of permutations with recurtion method" do
+      @obj_per = RubyAlgorithms::Permutation.new "1234", true
+      arr_permutations = @obj_per.turn
+      arr_permutations.length.should == 24
+      arr_permutations.should be_include("1243")
+      arr_permutations.should be_include("1423")
+      arr_permutations.should be_include("4123")
+      arr_permutations.should be_include("4132")
+    end
+    
+  end
+
+  describe "Array#move_by_direction" do
+    it "should move chose element by indication" do
+      arr = [[1,-1],[2,-1],[3,-1],[4,-1]]
+      var = arr.move_by_direction([4,-1],-1)
+      var.should be_true
+      arr.should == [[1,-1],[2,-1],[4,-1],[3,-1]]
+      arr.move_by_direction([4,-1],-1)
+      arr.should == [[1,-1],[4,-1],[2,-1],[3,-1]]
+      arr.move_by_direction([4,-1],-1)
+      arr.should == [[4,-1],[1,-1],[2,-1],[3,-1]]
+
+      arr.move_by_direction([4,-1],-1)
+      arr.should == [[4,1],[1,-1],[3,-1],[2,-1]]
+      arr.move_by_direction([4,1],1)
+      arr.should == [[1,-1],[4,1],[3,-1],[2,-1]]
+      arr.move_by_direction([4,1],1)
+      arr.should == [[1,-1],[3,-1],[4,1],[2,-1]]
+      arr.move_by_direction([4,1],1)
+      arr.should == [[1,-1],[3,-1],[2,-1],[4,1]]
+
+      arr.move_by_direction([4,1],1)
+      arr.should == [[3,-1],[1,-1],[2,-1],[4,-1]]
+      arr.move_by_direction([4,-1],-1)
+      arr.should == [[3,-1],[1,-1],[4,-1],[2,-1]]
+      arr.move_by_direction([4,-1],-1)
+      arr.should == [[3,-1],[4,-1],[1,-1],[2,-1]]
+      arr.move_by_direction([4,-1],-1)
+      arr.should == [[4,-1],[3,-1],[1,-1],[2,-1]]
+
+      arr.move_by_direction([4,-1],-1)
+      arr.should == [[4,1],[3,1],[2,-1],[1,-1]]
+      arr.move_by_direction([4,1],1)
+      arr.should == [[3,1],[4,1],[2,-1],[1,-1]]
+      arr.move_by_direction([4,1],1)
+      arr.should == [[3,1],[2,-1],[4,1],[1,-1]]
+      arr.move_by_direction([4,1],1)
+      arr.should == [[3,1],[2,-1],[1,-1],[4,1]]
+
     end
   end
   
 end
+
+
+
+
+
+ 
